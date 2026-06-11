ef12e22af3ae94fb50299600b07d348657234d98
Pooja Shivakumar <poojashivakumar451@gmail.com>
2026-06-12 05:17:24 +0530

    Updated some files

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBxsXSjGtDTnR3p9kWG50Rv9cl-3vp6AjM",
+  authDomain: "techmasters-innovations-drive.firebaseapp.com",
+  projectId: "techmasters-innovations-drive",
+  storageBucket: "techmasters-innovations-drive.firebasestorage.app",
+  messagingSenderId: "190856617063",
+  appId: "1:190856617063:web:a366f0c5f000a2ba55c8c2",
+  measurementId: "G-6F46TM1TPC"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export default app;
